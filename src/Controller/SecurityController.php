aadff69dc8409495f8f4a34bc8af43d9186e3ef1
Maxi Rojas <arnaldomrojas@outlook.com>
2024-10-30 15:54:55 +0100

    login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,22 +13,29 @@ class SecurityController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils, SectionRepository $sectionRepository): Response
     {
+        // Inicializar username como string vacío
+        $username = '';
+        
+        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-    $lastUsername = $authenticationUtils->getUsername();
-    $sections = $sectionRepository->findAll();
-    
-    return $this->render('login/login.html.twig', [
-        'controller_name' => 'SecurityController',
-        'sections' => $sections,
-        'username' => $username,
-        'error' => $error,
-    ]);
+        
+        // Si hay un username previo, lo usamos
+        if ($authenticationUtils->getLastUsername()) {
+            $username = $authenticationUtils->getLastUsername();
+        }
+        
+        // get sections for navigation
+        $sections = $sectionRepository->findAll();
+
+        return $this->render('security/index.html.twig', [
+            'username' => $username,
+            'error' => $error,
+            'sections' => $sections
+        ]);
     }
-    
 
-#[Route('/logout', name: 'app_logout')]
-
-public function logout(): void
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
